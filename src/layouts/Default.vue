780f0c484e7ac4468a64fca851f9f79d59df8cf6
Vantol Bennett <vvantol2000@icloud.com>
2022-02-05 23:36:29 -0500

    intial commit

---
+++ src/layouts/Default.vue
@@ -0,0 +1,20 @@
+<script setup>
+import EvalSidebar from "../components/EvalSidebar.vue";
+import EvalNavigation from "../components/EvalNavigation.vue";
+</script>
+<template>
+  <div class="flex flex-no-wrap bg-white">
+    <!-- Sidebar starts -->
+    <!-- Remove class [ hidden ] and replace [ sm:flex ] with [ flex ] -->
+    <EvalSidebar />
+    <!-- Sidebar ends -->
+    <div class="w-full">
+      <!-- Navigation starts -->
+      <EvalNavigation />
+      <!-- Navigation ends -->
+      <!-- Remove class [ h-64 ] when adding a card block -->
+      <slot />
+    </div>
+  </div>
+  <p>Footer Goes here</p>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import EvaluationFormOne from "../components/features/forms/EvaluationFormOne.vue";
+import Default from "../layouts/Default.vue";
+</script>
+
+<template>
+  <main>
+    <Default>
+      <div class="mx-auto py-10 h-full md:w-4/5 w-11/12 px-6">
+        <!-- Remove class [ border-dashed border-2 border-gray-300 ] to remove dotted border -->
+        <div class="w-full h-full rounded">
+          <EvaluationFormOne />
+          <!-- Place your content here -->
+        </div>
+      </div>
+    </Default>
+  </main>
+</template>
